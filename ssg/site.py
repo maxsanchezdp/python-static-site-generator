bbcaf9d521c7fd28d02a1c5f7ad25ed5ba956839
maxsanchezdp <maximiliano.sanchez@sdggroup.com>
2021-08-10 11:57:10 +0200

    fixed Site class

--- ssg/site.py
+++ ssg/site.py
@@ -8,7 +8,7 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest+"/"+path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
@@ -16,4 +16,4 @@ class Site:
 
         for path in self.source.rglob("*"):
             if path.is_dir():
-                Site.create_dir(path)
+                self.create_dir(path)
